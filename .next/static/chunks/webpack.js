affb6fff5f92f4a7602a6d75ccbdffd224cef17b
Bilal Bhatti <bilal@yobo.dev>
2020-10-10 19:02:49 +0500

    bug fixing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a94af2d30d26170c98bb";
+/******/ 	var hotCurrentHash = "f022ef78d98254f02c9a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
